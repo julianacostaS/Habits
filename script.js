a9ecd8419e461000d3a64400485ec2e53e947f44
Juliana Costa <julianacosta_s@hotmail.com>
2023-02-08 11:19:21 +1100

    :initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// Using external library
+const form = document.querySelector('#form-habits')
+const habitsApp = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+// to add a day on click
+button.addEventListener('click', add)
+    // to change and store the date
+form.addEventListener('change', save)
+
+function add() {
+
+    // get current date and take out the year by using .slice
+    // const today = new Date().toLocaleDateString().slice(0, -5)
+
+    const dayExists = habitsApp.dayExists(today)
+
+    // use an if statement to check whether the day has already been included.
+    if (dayExists) {
+        alert('Day already included! 🟡')
+        return
+    }
+
+    alert('Day successfully added! ✅')
+    habitsApp.addDay(today)
+}
+
+function save() {
+    // save and store the information in the local storage inside the browser.Give the localStorage a name.
+    // Convert the object to string by using JSON. stringfy
+    localStorage.setItem('Habits@Application', JSON.stringify(habitsApp.data))
+}
+
+// get the data from local storage, transform it back to an object by using the JSON.parse
+// || {} means that if the application can't find any data in the localStorage it will accept an empty object {}
+const data = JSON.parse(localStorage.getItem('Habits@Application')) || {}
+habitsApp.setData(data)
+habitsApp.load()
